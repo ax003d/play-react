f200d369228048848f47332222429303fbe0062d
rzheng <rzheng@xtremeprog.com>
2017-10-18 15:21:30 +0800

    use redux dev ext

--- src/Store.js
+++ src/Store.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import { reducer as toDoReduer } from './todos';
 import { reducer as filterReducer } from './filter';
 import { FILTER_ALL } from './constants';
+
+const win = window;
 
 const reducer = combineReducers({
     todo: toDoReduer,
     filter: filterReducer
 });
 
-export default createStore(reducer, {filter: FILTER_ALL});
+const middlewares = [];
+
+if (process.env.NODE_DEV !== 'production') {
+    // middlewares.push(require('redux-immutable-state-invariant'));
+}
+
+const storeEnhancers = compose(
+    applyMiddleware(...middlewares),
+    (win && win.devToolsExtension) ? win.devToolsExtension() : (f) => f,
+);
+
+export default createStore(reducer, {filter: FILTER_ALL}, storeEnhancers);
